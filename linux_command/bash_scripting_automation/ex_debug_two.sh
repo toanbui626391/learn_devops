8aa125a46353b23911ac29f93b3bb98ae21bff85
root <root@AC929999.localdomain>
2022-08-11 17:24:21 +0700

    update

--- linux_command/bash_scripting_automation/ex_debug_two.sh
+++ linux_command/bash_scripting_automation/ex_debug_two.sh
@@ -2,6 +2,8 @@
 
 #set -x will display each command step and the result of it.
 #set +x will close debug section
+#we can also use set command with flag like -e, stop execution when command exit with code different than 0
+#flag -v print shell input lines as they are read.
 TEST_VAR="test"
 set -x
 echo $TEST_VAR
