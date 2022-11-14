27119c2855585b4219fc8bd2ea575ecc44b3aa38
toanbui1991 <toanbui1991@gmail.com>
2022-11-14 10:31:58 +0700

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "3.5.0"
     }
   }
@@ -31,18 +31,34 @@ provider "google" {
 
   project = "airflow-gke-338120-352104"
   region  = "asia-southeast1"
-  zone    = "asia-southeast1-a"
+  zone    = "asia-southeast1-b"
 }
 
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-custom-mode-network"
+  auto_create_subnetworks = false
+  # mtu                     = 1460
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "my-custom-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "asia-southeast1"
+  network       = google_compute_network.vpc_network.id
+}
+# [END compute_flask_quickstart_vpc]
+
+# [START compute_flask_quickstart_vm]
+# Create a single Compute Engine instance
 resource "google_compute_instance" "default" {
   name         = "flask-vm"
   machine_type = "f1-micro"
-  zone         = "us-west1-a"
+  zone         = "asia-southeast1-b"
   tags         = ["ssh"]
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-minimal-2204-jammy-v20221101"
+      image = "debian-cloud/debian-11"
     }
   }
 
@@ -59,7 +75,56 @@ resource "google_compute_instance" "default" {
 }
 # [END compute_flask_quickstart_vm]
 
+# [START vpc_flask_quickstart_ssh_fw]
+resource "google_compute_firewall" "ssh" {
+  name = "allow-ssh"
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  direction     = "INGRESS"
+  network       = google_compute_network.vpc_network.id
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
+}
+# [END vpc_flask_quickstart_ssh_fw]
 
 
+# [START vpc_flask_quickstart_5000_fw]
+resource "google_compute_firewall" "flask" {
+  name    = "flask-app-firewall"
+  network = google_compute_network.vpc_network.id
 
+  allow {
+    protocol = "tcp"
+    ports    = ["5000"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+# [END vpc_flask_quickstart_5000_fw]
+
+# Create new multi-region storage bucket in the US
+# with versioning enabled
+
+# [START storage_bucket_tf_with_versioning]
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  force_destroy = false
+  location      = "US"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+}
+
+
+// A variable for extracting the external IP address of the VM
+output "Web-server-URL" {
+ value = join("",["http://",google_compute_instance.default.network_interface.0.access_config.0.nat_ip,":5000"])
+}
 
