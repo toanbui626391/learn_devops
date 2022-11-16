ed33b592a0e27a50e453d6c70988ea60dc57189d
toanbui1991 <toanbui1991@gmail.com>
2022-11-16 11:30:43 +0700

    update

--- terraform_data_engineer/main.tf
+++ terraform_data_engineer/main.tf
@@ -22,7 +22,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "3.5.0"
+      version = ">=4.41.0"
     }
   }
 }
@@ -30,78 +30,110 @@ terraform {
 provider "google" {
   credentials = file("../terraform/terraform_personal.json")
 
-  project = "airflow-gke-338120-352104"
-  region  = "asia-southeast1"
-  zone    = "asia-southeast1-b"
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
 }
 
-/**
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-# [START functions_v2_full]
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 4.34.0"
-    }
-  }
-}
 
 resource "random_id" "bucket_prefix" {
   byte_length = 8
 }
 
-# #create a service account with display_name, account_id is the account which used to login and general manager
-# #we manage service account seperately, do not need to create new service account
-# resource "google_service_account" "account" {
-#   account_id   = "gcf-sa"
-#   display_name = "Test Service Account"
+
+# data "google_storage_project_service_account" "gcs_account" {
 # }
 
-resource "google_pubsub_topic" "topic" {
-  name = "functions2-topic"
-}
 
-resource "google_storage_bucket" "bucket" {
+#create source bucket
+resource "google_storage_bucket" "source_bucket" {
   name                        = "${random_id.bucket_prefix.hex}-gcf-source" # Every bucket name must be globally unique
-  location                    = "US"
+  location                    = var.region
   uniform_bucket_level_access = true
 }
 
+#upload source code to a bucket
 resource "google_storage_bucket_object" "object" {
   name   = "function-source.zip"
-  bucket = google_storage_bucket.bucket.name
-  source = "function-source.zip" # Add path to the zipped function source code
+  bucket = google_storage_bucket.source_bucket.name
+  source = var.python_source
 }
 
+#create bucket which will trigger cloud function
+resource "google_storage_bucket" "trigger_bucket" {
+  name                        = "${random_id.bucket_prefix.hex}-gcf-trigger-bucket"
+  location                    = var.region
+  uniform_bucket_level_access = true
+}
+
+#get special service account of cloud storage
+data "google_storage_project_service_account" "gcs_account" {
+}
+
+#get project information
+data "google_project" "project" {
+}
+
+#binding publisher role for cloud storage service account at project level
+resource "google_project_iam_member" "gcs_pubsub_publishing" {
+  project = data.google_project.project.project_id
+  role    = "roles/pubsub.publisher"
+  member  = "serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"
+}
+
+#create service account with id gcf-sa for cloud function and eventarc
+resource "google_service_account" "account" {
+  account_id   = "gcf-sa"
+  display_name = "Test Service Account - used for both the cloud function and eventarc trigger in the test"
+}
+
+# grant role to member with google_project_iam_member
+# depend_on means we need other role-binding before this one
+# binding gcf-sa service account to role:
+#roles/run.invoker
+#roles/eventarc.eventReceiver
+#roles/artifactregistry.reader
+resource "google_project_iam_member" "invoking" {
+  project    = data.google_project.project.project_id
+  role       = "roles/run.invoker"
+  member     = "serviceAccount:${google_service_account.account.email}"
+  depends_on = [google_project_iam_member.gcs_pubsub_publishing]
+}
+
+resource "google_project_iam_member" "event_receiving" {
+  project    = data.google_project.project.project_id
+  role       = "roles/eventarc.eventReceiver"
+  member     = "serviceAccount:${google_service_account.account.email}"
+  depends_on = [google_project_iam_member.invoking]
+}
+
+resource "google_project_iam_member" "artifactregistry_reader" {
+  project    = data.google_project.project.project_id
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:${google_service_account.account.email}"
+  depends_on = [google_project_iam_member.event_receiving]
+}
+
+
+#create cloud function
 resource "google_cloudfunctions2_function" "function" {
+  depends_on = [
+    google_project_iam_member.event_receiving,
+    google_project_iam_member.artifactregistry_reader,
+  ]
   name        = "function"
-  location    = "us-central1"
+  location    = var.region
   description = "a new function"
 
   build_config {
-    runtime     = "nodejs16"
-    entry_point = "helloPubSub" # Set the entry point
+    runtime     = var.runtime
+    entry_point = var.entry_point # Set the entry point in the code
     environment_variables = {
       BUILD_CONFIG_TEST = "build_test"
     }
     source {
       storage_source {
-        bucket = google_storage_bucket.bucket.name
+        bucket = google_storage_bucket.source_bucket.name
         object = google_storage_bucket_object.object.name
       }
     }
@@ -117,14 +149,24 @@ resource "google_cloudfunctions2_function" "function" {
     }
     ingress_settings               = "ALLOW_INTERNAL_ONLY"
     all_traffic_on_latest_revision = true
-    service_account_email          = var.terraform_service_account
+    service_account_email          = google_service_account.account.email
   }
 
   event_trigger {
-    trigger_region = "us-central1"
-    event_type     = "google.cloud.pubsub.topic.v1.messagePublished"
-    pubsub_topic   = google_pubsub_topic.topic.id
-    retry_policy   = "RETRY_POLICY_RETRY"
+    trigger_region        = var.region # The trigger must be in the same location as the bucket
+    event_type            = var.gcs_event
+    retry_policy          = "RETRY_POLICY_RETRY"
+    service_account_email = google_service_account.account.email
+    event_filters {
+      attribute = "bucket"
+      value     = google_storage_bucket.trigger_bucket.name
+    }
   }
 }
 # [END functions_v2_full]
+
+
+
+
+
+
--- terraform_data_engineer/variables.tf
+++ terraform_data_engineer/variables.tf
@@ -1,4 +1,52 @@
-variable "terraform_service_account" {
-    type = string
-    default = "terraform-personal@airflow-gke-338120-352104.iam.gserviceaccount.com"
+
+#project config
+variable "project_id" {
+  type    = string
+  default = "airflow-gke-338120-352104"
 }
+
+variable "region" {
+  type    = string
+  default = "asia-southeast1"
+}
+
+variable "zone" {
+  type    = string
+  default = "asia-southeast1-b"
+}
+
+variable "terraform_service_account" {
+  type    = string
+  default = "terraform-personal@airflow-gke-338120-352104.iam.gserviceaccount.com"
+}
+
+#storage config
+variable "bucket_location" {
+  type    = string
+  default = "asia-southeast1"
+}
+
+
+#function config
+variable "runtime" {
+  type    = string
+  default = "python38"
+}
+
+variable "python_source" {
+  type    = string
+  default = "./python-docs-samples/functions/v2/storage/function-source.zip"
+
+}
+#function to run in the source code
+variable "entry_point" {
+  type    = string
+  default = "hello_gcs"
+}
+
+variable "gcs_event" {
+  type    = string
+  default = "google.cloud.storage.object.v1.finalized"
+}
+
+
