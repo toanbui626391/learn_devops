71f9f092da669a7c0ecd3de116321bc670d7abcf
toanbui626391 <toanbui626391@gmail.com>
2023-07-28 15:08:39 +0700

    update

---
+++ terraform_data_engineer/cloud_build/python_docker/readme.md
@@ -0,0 +1,8 @@
+##using cloud build or python application
+    - reference link: https://cloud.google.com/build/docs/building/build-containerize-python
+    - github reference link: https://github.com/GoogleCloudPlatform/cloud-build-samples/tree/main/python-example-flask
+
+##step
+    - one: create bucket to store log
+        - gsutil mb -c standard -l us-east1 gs://toanbui1991-python-logs
+
--- tf-tutorial/main.tf
+++ tf-tutorial/main.tf
@@ -6,92 +6,17 @@ provider "google" {
   zone    = "us-west1-a"
 }
 
-# [START compute_flask_quickstart_vpc]
-resource "google_compute_network" "vpc_network" {
-  name                    = "my-custom-mode-network"
-  auto_create_subnetworks = false
-  mtu                     = 1460
-}
+resource "google_cloud_identity_group" "cloud_identity_group_basic" {
+  display_name         = "my-identity-group"
+  initial_group_config = "WITH_INITIAL_OWNER"
 
-resource "google_compute_subnetwork" "default" {
-  name          = "my-custom-subnet"
-  ip_cidr_range = "10.0.1.0/24"
-  region        = "us-west1"
-  network       = google_compute_network.vpc_network.id
-}
-# [END compute_flask_quickstart_vpc]
+  parent = "customers/A01b123xz"
 
-# [START compute_flask_quickstart_vm]
-# Create a single Compute Engine instance
-resource "google_compute_instance" "default" {
-  name         = "flask-vm"
-  machine_type = "f1-micro"
-  zone         = "us-west1-a"
-  tags         = ["ssh"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
+  group_key {
+      id = "my-identity-group@example.com"
   }
 
-  # Install Flask
-  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
-
-  network_interface {
-    subnetwork = google_compute_subnetwork.default.id
-
-    access_config {
-      # Include this section to give the VM an external IP address
-    }
+  labels = {
+    "cloudidentity.googleapis.com/groups.discussion_forum" = ""
   }
 }
-# [END compute_flask_quickstart_vm]
-
-# [START vpc_flask_quickstart_ssh_fw]
-resource "google_compute_firewall" "ssh" {
-  name = "allow-ssh"
-  allow {
-    ports    = ["22"]
-    protocol = "tcp"
-  }
-  direction     = "INGRESS"
-  network       = google_compute_network.vpc_network.id
-  priority      = 1000
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["ssh"]
-}
-# [END vpc_flask_quickstart_ssh_fw]
-
-
-# [START vpc_flask_quickstart_5000_fw]
-resource "google_compute_firewall" "flask" {
-  name    = "flask-app-firewall"
-  network = google_compute_network.vpc_network.id
-
-  allow {
-    protocol = "tcp"
-    ports    = ["5000"]
-  }
-  source_ranges = ["0.0.0.0/0"]
-}
-# [END vpc_flask_quickstart_5000_fw]
-
-# Create new multi-region storage bucket in the US
-# with versioning enabled
-
-# [START storage_bucket_tf_with_versioning]
-resource "random_id" "bucket_prefix" {
-  byte_length = 8
-}
-
-resource "google_storage_bucket" "default" {
-  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
-  force_destroy = false
-  location      = "US"
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-}
-# [END storage_bucket_tf_with_versioning]
